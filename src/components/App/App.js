c7cd99c8fe579657e63806cf7eec0cc27607047f
Sharmarke Ahmed <somaliwarsan@gmail.com>
2020-06-18 14:45:34 +0300

    updated app.js to functional component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import 'bootstrap';
@@ -8,21 +7,14 @@ import HomePage from '../../pages/homepage/homepage';
 import MealInfo from '../../pages/meal-info/meal-info';
 import Header from '../header/header';
 
-class App extends React.Component {
-  componentDidMount() {
-  }
+const App = () => (
+  <Router>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/mealInfo" component={MealInfo} />
+    </Switch>
+  </Router>
+);
 
-  render() {
-    return (
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/mealInfo" component={MealInfo} />
-        </Switch>
-      </Router>
-    );
-  }
-}
-
-export default connect(null)(App);
+export default App;
